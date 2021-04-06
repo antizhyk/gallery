2cd23e0068b2d6dd0763339239be8264d131c530
antizyk <01antizyk.it@gmail.com>
2021-04-06 21:43:24 +0300

    Создал модальное окно

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Gallery-base
 const midImg = document.querySelector('.gallery__mid');
 const smallImgs = document.querySelectorAll('.gallery__small');
 const gallItem = document.querySelectorAll('.gallery__item');
@@ -17,4 +18,38 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 })
+//===========================================
+//Popup
+midImg.addEventListener('click', () => {
+    const modal = document.createElement('div');
+    modal.classList.add('modal')
+    modal.innerHTML = `
+    <div class="modal__wrapp">
+        <div class="modal__content">
+        </div>
+    </div>
+    `;
+    if (!document.querySelector('.modal')) {
+        document.body.append(modal);
+        document.body.style.overflow = 'hidden';
+        document.body.style.paddingRight = '15px';
+        setTimeout(() => {
+            modal.classList.add('show');
+        }, 10)
 
+        modal.addEventListener('click', e => {
+            let target = e.target;
+            if (target.classList.contains('modal__wrapp')) {
+                modal.classList.remove('show');
+                setTimeout(() => {
+                    document.body.removeChild(modal);
+                    document.body.style.overflow = 'auto';
+                    document.body.style.paddingRight = '0px';
+                }, 300)
+
+            }
+        })
+    }
+
+})
+//=================================
